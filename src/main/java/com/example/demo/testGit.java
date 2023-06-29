e7362aec0b18d03b93f7ffa07f4ad9037d65bf84
jiguanggui <2281633841@qq.com>
2023-06-29 13:58:30 +0800

    test.add

---
+++ src/main/java/com/example/demo/testGit.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * @Description:
+ * @Author: Ji GuangGui
+ * @Date: 2023-06-29-13:56
+ */
+public class testGit {
+    private String GIt;
+    private String GIt1;
+    private  Integer Git12;
+    private String GIt2;
+}
