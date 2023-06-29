e0d243ae4fff859d021822991a4fd4b93ebb882f
jiguanggui <2281633841@qq.com>
2023-06-29 15:15:24 +0800

    test.add0.5

--- src/main/java/com/example/demo/testGit.java
+++ src/main/java/com/example/demo/testGit.java
@@ -10,11 +10,16 @@ public class testGit {
    private Double Git23;
     private String GIt2;
 
-
     private String GitTest1;
     private String GitTest2;
     private String GitTest3;
     private String GitTest4;
     private String GitTest5;
+    private String GitTest6;
+    public static void main(String[] args) {
+        System.out.println("我是test");
+    }
+
+
 
 }
